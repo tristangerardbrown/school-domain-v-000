eff02b7de72dc9259e454d10b3fe51a2b79d364c
Tristan Brown <tgbrown@stanford.edu>
2018-09-02 06:41:41 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -4,15 +4,17 @@ attr_reader :school_name
 
   def initialize(school_name)
     @school_name=school_name
+    @roster = {}
   end
 
   def roster
-    {}
+    @roster
   end
 
   def add_student(name,grade)
-    roster << "{grade => name}"
-    roster
+    @student_name = name
+    @student_grade = grade
+    roster[grade] << name 
 end
 
 
