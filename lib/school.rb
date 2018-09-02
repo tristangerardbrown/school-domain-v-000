9e5b4c9151999e91e1c76a76fb8d173f3490d4ba
Tristan Brown <tgbrown@stanford.edu>
2018-09-02 06:42:03 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -12,9 +12,7 @@ attr_reader :school_name
   end
 
   def add_student(name,grade)
-    @student_name = name
-    @student_grade = grade
-    roster[grade] << name 
+    roster[grade] << name
 end
 
 
